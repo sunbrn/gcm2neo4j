48ddad1b9d57d5e455b409c6b6ac32ee41c0b93a
anna bernasconi <bernasconi.ann@gmail.com>
2018-12-13 13:53:24 +0100

    fixes

---
+++ create_indexes.py
@@ -0,0 +1,36 @@
+from neo4jrestclient.client import GraphDatabase
+
+graph = GraphDatabase("http://neo4j:yellow@geco.deib.polimi.it:7474/db/data/")
+tx = graph.transaction(for_query=True)
+
+donor_index = "CREATE INDEX ON :Donor(donor_source_id)"
+tx.append(donor_index)
+tx.commit()
+
+biosample_index = "CREATE INDEX ON :Biosample(biosample_source_id)"
+tx.append(biosample_index)
+tx.commit()
+
+replicate_index = "CREATE INDEX ON :Replicate(replicate_source_id)"
+tx.append(replicate_index)
+tx.commit()
+
+item_index = "CREATE INDEX ON :Item(item_source_id)"
+tx.append(item_index)
+tx.commit()
+
+case_index = "CREATE INDEX ON :Case(case_source_id)"
+tx.append(case_index)
+tx.commit()
+
+pair_index_key = "CREATE INDEX ON :Pair(key)"
+tx.append(pair_index_key)
+tx.commit()
+
+pair_index_value = "CREATE INDEX ON :Pair(value)"
+tx.append(pair_index_value)
+tx.commit()
+
+pair_index_key_value = "CREATE INDEX ON :Pair(key,value)"
+tx.append(pair_index_key_value)
+tx.commit()
